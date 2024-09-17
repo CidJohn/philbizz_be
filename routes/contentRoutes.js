1e855d6fc562b4d23ae2dbcbb7136a04591c5880
Cidjohn <cidjohn31@gmail.com>
2024-09-17 15:34:32 +0800

    company settings - time_desc

--- routes/contentRoutes.js
+++ routes/contentRoutes.js
@@ -58,17 +58,19 @@ router.get("/card_path", cardPath);
 router.get("/card_info/:type", cardInfo);
 router.get("/business-data", getBusinessData);
 router.get("/imageURL/:type", imageURL);
+
+//company
 router.get("/homeview-business", getHomeViewBusiness);
 router.get("/business-settings", getCompanySettings);
 router.get("/business-category", getBusinessCategories);
 router.get("/business-company-filter", companyFilter);
-
 router.get("/business-company-viewpage", getbusinessCompanyView);
 router.get("/business-company-images", getCompany_Image);
 router.get("/business-company-personnel", getCompany_personnel);
 router.get("/business-company-product", getCompany_product);
 router.get("/business-company-socials", getCompany_social)
 
+//Blog
 router.get("/blog-settings", BlogSettings);
 router.get("/blog-content", BlogContent);
 router.get("/comment-section", BlogComment);
